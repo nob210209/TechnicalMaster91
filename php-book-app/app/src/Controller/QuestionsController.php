28c4cd5a466e4108103066250dcec03fd86b4d29
nob210209 <s1210209ryot@gmail.com>
2019-04-20 22:09:46 +0900

    質問一覧画面のコントローラー追加

---
+++ php-book-app/app/src/Controller/QuestionsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace  APP\Controller;
+
+/**
+ * Questions Controller
+ */
+class QuestionsController extends AppController
+{
+    /**
+     * @inheritdoc
+     */
+    public function initialize()
+    {
+        parent::initialize();
+    }
+
+    /**
+     * 質問一覧画面
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        //ここに処理を書いていく
+    }
+}

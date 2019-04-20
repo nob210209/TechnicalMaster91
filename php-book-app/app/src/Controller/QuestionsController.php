1f99b8eee6b9d1c4b1d5058927d254c5c4c0d7de
nob210209 <s1210209ryot@gmail.com>
2019-04-21 00:34:42 +0900

    質問投稿画面のビューとコントローラーの処理追加

--- php-book-app/app/src/Controller/QuestionsController.php
+++ php-book-app/app/src/Controller/QuestionsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace  APP\Controller;
+namespace  App\Controller;
 
 /**
  * Questions Controller
@@ -27,5 +27,29 @@ class QuestionsController extends AppController
         ]);
 
         $this->set(compact('questions'));
+    }
+
+    /**
+     * 質問投稿画面/質問投稿処理
+     *
+     * @return \Cake\Http\Response|null 質問投稿後に質問一覧画面に遷移する
+     */
+    public function add()
+    {
+        $question = $this->Questions->newEntity();
+
+        if ($this->request->is('post')) {
+            $question = $this->Questions->pathEntity($question, $this->request->getData());
+            $question->user_id = 1; //todoユーザー管理機能実装時に修正する
+
+            if ($this->Questions->save($question)) {
+                $this->Flash->success('質問を投稿しました');
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error('質問の投稿に失敗しました');
+        }
+
+        $this->set(compact('question'));
     }
 }

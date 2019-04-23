bb7de62f21b7e7e3e3b00b31431c883c7899f8d0
nob210209 <s1210209ryot@gmail.com>
2019-04-24 02:00:04 +0900

    ユーザー編集画面の追加

--- php-book-app/app/src/Controller/UsersController.php
+++ php-book-app/app/src/Controller/UsersController.php
@@ -38,5 +38,27 @@ class UsersController extends AppController
         $this->set(compact('user'));
     }
 
+    /**
+     * ユーザー編集画面/ユーザー情報更新画面
+     *
+     * @return \Cake\Http\Response|null ユーザー情報更新後に質問一覧画面へ遷移する
+     */
+    public function edit()
+    {
+        $user = $this->Users->get($this->Auth->user('id'));
+        if ($this->request->is('put')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Auth->setUser($user->toArray());
+
+                $this->Flash->success('ユーザー情報を更新しました');
+
+                return $this->redirect(['controller' => 'Questions', 'action' => 'index']);
+            }
+            $this->Flash->error('ユーザー情報の更新に失敗しました');
+        }
+        $this->set(compact('user'));
+    }
+
 
 }

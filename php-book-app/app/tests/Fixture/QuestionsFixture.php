31603b1689fe4ccb6071faa0ad5b031097c17ac1
nob210209 <s1210209ryot@gmail.com>
2019-05-19 21:39:38 +0900

    questionsテーブルのフィクスチャークラスを作成

---
+++ php-book-app/app/tests/Fixture/QuestionsFixture.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wtanabe-ryota
+ * Date: 2019/05/19
+ * Time: 21:13
+ */
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * QuestionsFixture
+ *
+ */
+class QuestionsFixture extends TestFixture
+{
+    /**
+     * テーブルのフィールド情報
+     *
+     * @var array
+     */
+    //@codingStandardsIgnoreStart
+    public $fields = [
+        'id' => [
+            'type' => 'integer',
+            'length' => 10,
+            'unsigned' => true,
+            'null' => false,
+            'default' => null,
+            'comment' => '',
+            'autoIncrement' => true,
+            'precision' => null
+        ],
+        'user_id' => [
+            'type' => 'integer',
+            'length' => 10,
+            'unsigned' => true,
+            'null' => false,
+            'default' => null,
+            'comment' => '',
+            'autoIncrement' => null,
+            'precision' => null
+        ],
+        'body' => [
+            'type' => 'string',
+            'length' => 255,
+            'null' => false,
+            'default' => null,
+            'collate' => 'utf8mb4_general_ci',
+            'comment' => '',
+            'precision' => null,
+            'fixed' => null
+        ],
+        'crested' => [
+            'type' => 'datetime',
+            'length' => null,
+            'null' => false,
+            'default' => null,
+            'comment' => '',
+            'precision' => null
+        ],
+        'modified' => [
+            'type' => 'datetime',
+            'length' => null,
+            'null' => false,
+            'default' => null,
+            'comment' => '',
+            'precision' => null
+        ],
+        '_constraints' => [
+            'primary' => [
+                'type' => 'primary',
+                'columns' => ['id'],
+                'length' => []
+            ]
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collection' => 'utf8mb4_general_ci',
+        ]
+    ];
+    //@codingStandardsIgnoreEnd
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'body' => '好きなPHPのフレームワークは何ですか？',
+                'created' => '2018-12-01 13:00:00',
+                'modified' => '2018-12-01 13:00:00'
+            ],
+            [
+                'id' => 2,
+                'user_id' => 2,
+                'body' => '好きなPHPの関数は何ですか？',
+                'created' => '2018-12-02 14:00:00',
+                'modified' => '2018-12-02 14:00:00'
+            ]
+        ];
+        parent::init();
+    }
+}
